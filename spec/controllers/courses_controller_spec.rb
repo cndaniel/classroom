dd40d4844e39b1a23948acfa269f83826be71b6c
cndaniel <cndaniel@me.com>
2017-03-01 23:47:10 +0100

    split into 2 examples

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
   describe "GET index" do
-    it "assigns @courses and render template" do
+    it "assigns @courses" do
     course1 = create(:course)
     course2 = create(:course)
 
     get :index
 
     expect(assigns[:courses]).to eq([course1, course2])
-    expect(response).to render_template("index")
+
+    end
+    it "render template" do
+      course1 = create(:course)
+      course2 = create(:course)
+      get :index 
+      expect(response).to render_template("index")
   end
 end
 end
